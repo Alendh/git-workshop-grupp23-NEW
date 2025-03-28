e9bef41a96494ae13ff7fe21ce8865b680dc63e4
Bjornheimer <christoffer.bjornheimer@gmail.com>
2025-03-28 10:13:40 +0100

    Create BirgittaEmanuel.java

---
+++ src/BirgittaEmanuel.java
@@ -0,0 +1,6 @@
+public class BirgittaEmanuel {
+    public BirgittaEmanuel(){
+        System.out.println("This is my class, there are many like it but this is mine. Without my class" +
+                "I am nothing. without me, my class is nothing");
+    }
+}
