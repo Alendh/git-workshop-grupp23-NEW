524d9cf55f7ac6c2d122e2fc8a5012b22ed8bb72
alendh <alendawat.h@gmail.com>
2025-03-28 10:26:17 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("write something: ");
-        System.out.println("your wrote: " + scanner.nextLine());
+        System.out.println("Skriv klass namn: ");
+
+        String input = scanner.nextLine();
+        switch (input) {
+            case "Alend":
+                new Alend();
+                break;
+            case "BirgittaEmanuel":
+                new BirgittaEmanuel();
+                break;
+            case "Nini":
+                new Nini();
+                break;
+            case "SuperEpicClass":
+                new SuperEpicClass();
+                break;
+            default:
+                System.out.println("Klassen existerar inte");
+                break;
+        }
     }
 }
